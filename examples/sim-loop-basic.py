7d0d771452b308736794fc54b0c231f1c2b00baf
Efe Bastug <efe.bastug608@gmail.com>
2026-02-08 22:27:53 +0300

    Some library and classes
    Time library of python is added. Also, I added the numpy library, as in the main simulation, and imported pymunk into the 2D vector class. To determine the force applied to the cell segments at each step, I selected the number 1 as the sequence for the np.random.seed(1) function, which returns a number in a specific order and is outside the Simulator class. I imported the Simulator, CellConfig, and PhysicsConfig classes from the main source code of the project.

---
+++ examples/sim-loop-basic.py
@@ -0,0 +1,9 @@
+import time
+
+import numpy as np
+from pymunk import Vec2d
+
+from symbac.simulation import Simulator
+from symbac.simulation.config import CellConfig, PhysicsConfig
+
+np.random.seed(1)
