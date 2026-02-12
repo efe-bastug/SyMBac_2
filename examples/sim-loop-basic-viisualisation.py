e0fbe92d116a18b596f927d4009f58558548a042
Efe Bastug <efe.bastug608@gmail.com>
2026-02-12 19:45:23 +0300

    Importation of SimViewerConfig and LiveVisualisation that  sim-loop-basic doesn't include
    I imported the class LiveVisualisation from a document called live_visualisation.py, and the class of SimViewerConfig from a document called config.py in the main project.

--- examples/sim-loop-basic-viisualisation.py
+++ examples/sim-loop-basic-viisualisation.py
@@ -4,3 +4,6 @@ import numpy as np
 from pymunk import Vec2d
 
 from symbac.simulation import Simulator
+from symbac.simulation.config import CellConfig, PhysicsConfig, SimViewerConfig
+from symbac.simulation.visualisation.live.live_visualisation import LiveVisualisation
+
